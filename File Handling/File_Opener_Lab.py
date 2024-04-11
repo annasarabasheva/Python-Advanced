be9fb538e23118b5169bd8c05f7dad6a593b7292
Anna Sarabasheva <162803681+annasarabasheva@users.noreply.github.com>
2024-04-11 10:19:24 +0300

    Delete File Handling directory

--- File Handling/File_Opener_Lab.py
+++
@@ -1,8 +0,0 @@
-import os
-
-path_to_root = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(path_to_root, "text.txt")
-if os.path.isfile(file_path):
-    print("File found")
-else:
-    print("File not found")
--- File Handling/File_Reader_Lab.py
+++
@@ -1,8 +0,0 @@
-import os
-
-path_to_root = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(path_to_root, "numbers.txt")
-
-with open(file_path, "r") as file:
-    list_numbers = [int(el) for el in file.read().split()]
-    print(sum(list_numbers))
